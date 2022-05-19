7dcc1354e89b5536a076f6643474fdc70d3bd9ca
PROFAY <Oyewaletimmy01@gmail.com>
2022-05-19 01:24:34 +0000

    fds

---
+++ 0-betty.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * findBuiltIns - validates if command is builtin and executes
+ * @build: input build
+ * Return: 1 if found, 0 if not
+ */
+int findBuiltIns(config *build)
+{
+	register int i = 0;
+
+    return(0)
+}
